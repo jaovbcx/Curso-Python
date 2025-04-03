5fc1690ce7fc983974da04ea436679f5bed045b6
jaovbcx <v.conde@academico.ifrr.edu.br>
2025-04-02 21:42:04 -0400

    aula-2

--- bloco-1/aula-1.py
+++ bloco-1/aula-1.py
@@ -8,8 +8,12 @@ Para comentário:
 Possíveis erros em python:
 - Não colocar linha em branco no final
 - Não deixar dois espaços entre o código e o comentário em #
+- Python faz diferenciação de minúsculas e maiúsculas
 
 """
 
 print('Hello World')  # Imprime mensagem na tela.
 print('Python is a High Level programming language') # Imprime segundo texto na tela.
+print('João', 'Victor') # Quando há dois argumentos, automaticamente é colocado espaço entre as strings.
+print('João', 'Victor', sep='-') # sep muda o espaço por traço.
+print('João', 'Victor', sep='-', end=' Bezerra') # muda o final, colocado como padrão uma quebra de linha.
