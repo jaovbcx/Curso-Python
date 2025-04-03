2040239b06246ed05410e18599f865347c405824
jaovbcx <v.conde@academico.ifrr.edu.br>
2025-04-02 21:19:06 -0400

    primeira-aula

---
+++ bloco-1/aula-1.py
@@ -0,0 +1,15 @@
+"""
+
+Para comentário: 
+- #
+- " (3 vezes ao início e ao final)
+- ' (3 vezes ao início e ao final)
+
+Possíveis erros em python:
+- Não colocar linha em branco no final
+- Não deixar dois espaços entre o código e o comentário em #
+
+"""
+
+print('Hello World')  # Imprime mensagem na tela.
+print('Python is a High Level programming language') # Imprime segundo texto na tela.
