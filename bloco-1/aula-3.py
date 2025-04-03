9a800dbd9edf975c5e7c4e14a087e5505bda96c5
jaovbcx <v.conde@academico.ifrr.edu.br>
2025-04-02 21:57:49 -0400

    aula 3

---
+++ bloco-1/aula-3.py
@@ -0,0 +1,10 @@
+"""
+(str) = string - Cadeia de caracteres
+São lidos em aspas duplas (") ou simples (')
+\n - comando de quebrar linha
+para imprimir uma das aspas pode se inverter o uso delas no print ou usar caractere de escape (\) 
+para quebrar caractere de escape usa-se o operador r antes das aspas.
+"""
+
+print('Isso é uma aspas duplas " " ')
+print("Isso é uma aspas simples ' ' ")
